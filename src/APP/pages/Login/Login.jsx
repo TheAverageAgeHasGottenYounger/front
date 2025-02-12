9f776d349e7fe00e2c0b95ee7482c644f06a4921
nunomi0 <ykys1@naver.com>
2025-02-13 00:18:55 +0900

    디자인 시스템 추가(색상, 폰트, 글씨 크기)

--- src/APP/pages/Login/Login.jsx
+++ src/APP/pages/Login/Login.jsx
@@ -1,75 +1,76 @@
 import React, { useState, useEffect, useContext  } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as itemS from "./Styled/Login.styles"
-// import { ACCESS_TOKEN } from '../../Api/request';
-// import axios from 'axios';
+import { ACCESS_TOKEN } from '../../Api/request';
+import axios from 'axios';
 
 export default function Login() {
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // // const { confirm } = useContext(ConfirmContext);
-  // const { alert } = useContext(AlertContext);
-  // const [isAlertOpen, setIsAlertOpen] = useState(false);  // Alert 창 열림 여부
+  // const { confirm } = useContext(ConfirmContext);
+  const { alert } = useContext(AlertContext);
+  const [isAlertOpen, setIsAlertOpen] = useState(false);  // Alert 창 열림 여부
 
-	// const [email, setEmail] = useState('');
-	// const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  // useEffect(() => {
-  //   const handleKeyDown = (event) => {
-  //     if (event.key === 'Enter' && !isAlertOpen) { // Alert 창이 열려있지 않을 때만 실행
-  //       event.preventDefault();
-  //       handleSubmit();
-  //     }
-  //   };
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter' && !isAlertOpen) { // Alert 창이 열려있지 않을 때만 실행
+        event.preventDefault();
+        handleSubmit();
+      }
+    };
 
-  //   window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keydown', handleKeyDown);
 
-  //   return () => {
-  //     window.removeEventListener('keydown', handleKeyDown);
-  //   };
-  // }, [email, password, isAlertOpen]);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [email, password, isAlertOpen]);
 
-  // // 로그인 버튼
-  // const handleSubmit = async () => {
+  // 로그인 버튼
+  const handleSubmit = async () => {
    
-  //   const requestData = {
-  //     email: email,
-  //     password: password,
-  //   };
-  //   try {
-  //     const response = await axios.post(`${process.env.REACT_APP_API_URL}/member/login`, requestData);
-  //     console.log("response",response);
-  //     localStorage.setItem(ACCESS_TOKEN, response.data.result.accessToken);
-  //     if (response.data["isSuccess"]) {
-  //       console.log("로그인 성공!");
-  //       // navigate("/");
-  //       // window.location.reload('/');
-  //       window.location.replace('/');
-  //     } else {
-  //       // console.error("로그인 실패:", response.data);
-  //       setIsAlertOpen(true);
-  //       alert(response.data.message || "로그인 실패")
-  //       .then(() => {
-  //         setIsAlertOpen(false);
-  //       }); // 실패 메시지가 없으면 기본 메시지 표시
-  //     }
-  //   } catch (error) {
-  //     setIsAlertOpen(true);
-  //     const errorMessage = error.response?.data?.result?.message || error.response?.data?.result?.email || error.response?.data?.result?.password || error.response?.data?.result || error.response?.data?.message || "로그인 오류 발생"; // 객체를 문자열로 변환하거나 기본 메시지 사용
-  //     alert(String(errorMessage))  // 문자열로 변환 보장
-  //     .then(() => {
-  //       setIsAlertOpen(false);
-  //     }); // 실패 메시지가 없으면 기본 메시지 표시
-  //     console.error("로그인 오류:", error);
-  //   }
-  // };
+    const requestData = {
+      email: email,
+      password: password,
+    };
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/member/login`, requestData);
+      console.log("response",response);
+      localStorage.setItem(ACCESS_TOKEN, response.data.result.accessToken);
+      if (response.data["isSuccess"]) {
+        console.log("로그인 성공!");
+        // navigate("/");
+        // window.location.reload('/');
+        window.location.replace('/');
+      } else {
+        // console.error("로그인 실패:", response.data);
+        setIsAlertOpen(true);
+        alert(response.data.message || "로그인 실패")
+        .then(() => {
+          setIsAlertOpen(false);
+        }); // 실패 메시지가 없으면 기본 메시지 표시
+      }
+    } catch (error) {
+      setIsAlertOpen(true);
+      const errorMessage = error.response?.data?.result?.message || error.response?.data?.result?.email || error.response?.data?.result?.password || error.response?.data?.result || error.response?.data?.message || "로그인 오류 발생"; // 객체를 문자열로 변환하거나 기본 메시지 사용
+      alert(String(errorMessage))  // 문자열로 변환 보장
+      .then(() => {
+        setIsAlertOpen(false);
+      }); // 실패 메시지가 없으면 기본 메시지 표시
+      console.error("로그인 오류:", error);
+    }
+  };
   return (
-    <></>
-      // <itemS.Container>
-      //   <itemS.InnerContainer>
+    <><
+      <itemS.Container>
+        <itemS.InnerContainer>
           
-      //   </itemS.InnerContainer>
-      // </itemS.Container>
+        </itemS.InnerContainer>
+      </itemS.Container>
+      
   )
 }
---
+++ src/tokens.js
@@ -0,0 +1,84 @@
+export const colors = {
+  gray: {
+    900: "#1B1B1B",
+    800: "#2B2B2B",
+    700: "#414141",
+    600: "#686868",
+    500: "#808080",
+    400: "#979797",
+    300: "#C1C1C1",
+    200: "#DEDEDE",
+    100: "#F2F2F2",
+    50: "#FAFAFA",
+  },
+  primary: {
+    0: "#FF8D3C",
+    lighten: {
+      100: "#FF964D",
+      200: "#FFB8A8",
+      300: "#FFCCAB",
+      400: "#FFEEE3",
+    },
+    darken: {
+      100: "#F76B18",
+      200: "#BE5104",
+    },
+  },
+};
+
+export const fonts = {
+  primary: "Pretendard, sans-serif",
+};
+
+export const typography = {
+  h1: {
+    fontSize: "28px",
+    fontWeight: "700",
+    lineHeight: "140%",
+  },
+  h2: {
+    fontSize: "26px",
+    fontWeight: "700",
+    lineHeight: "140%",
+  },
+  h3: {
+    fontSize: "22px",
+    fontWeight: "700",
+    lineHeight: "140%",
+  },
+  bodyM: {
+    fontSize: "18px",
+    fontWeight: "500",
+    lineHeight: "140%",
+  },
+  bodyS: {
+    fontSize: "16px",
+    fontWeight: "500",
+    lineHeight: "140%",
+  },
+  buttonSb16: {
+    fontSize: "16px",
+    fontWeight: "600",
+    lineHeight: "140%",
+  },
+  buttonSb18: {
+    fontSize: "18px",
+    fontWeight: "600",
+    lineHeight: "140%",
+  },
+  buttonB18: {
+    fontSize: "18px",
+    fontWeight: "700",
+    lineHeight: "140%",
+  },
+  labelSb16: {
+    fontSize: "16px",
+    fontWeight: "600",
+    lineHeight: "140%",
+  },
+  labelSb14: {
+    fontSize: "14px",
+    fontWeight: "600",
+    lineHeight: "140%",
+  },
+};
